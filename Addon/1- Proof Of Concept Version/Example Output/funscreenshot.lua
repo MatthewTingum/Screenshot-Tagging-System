c92d911a39310737094293eee0c0b6bcab4083c2
kaylobadboy <kristopher.r.lommen@my.und.edu>
2016-11-29 21:41:59 -0600

    Example output Added for POC Version
    An example log file added to demonstrate how the file will be available
    for reading from other elements of the project (The Local App)

---
+++ Addon/1- Proof Of Concept Version/Example Output/funscreenshot.lua
@@ -0,0 +1,11 @@
+
+screenshotDB = {
+	"SESSION START AT 11/21/16 00:18:26 ../World of Warcraft/Interface/AddOns/funscreenshot/hello.lua ", -- [1]
+	"pic of me just starting|World of Warcraft/Screenshots/WoWScrnShot_112116_001826.jpg|11/21/16 00:18:26|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [2]
+	"pic of me looking at a young thistle boar|World of Warcraft/Screenshots/WoWScrnShot_112116_001840.jpg|11/21/16 00:18:40|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [3]
+	"im really bored|World of Warcraft/Screenshots/WoWScrnShot_112116_001851.jpg|11/21/16 00:18:51|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [4]
+	"this guy |World of Warcraft/Screenshots/WoWScrnShot_112116_001904.jpg|11/21/16 00:19:04|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [5]
+	"SESSION START AT 11/21/16 00:19:32 ../World of Warcraft/Interface/AddOns/funscreenshot/hello.lua ", -- [6]
+	"trolling wolf|World of Warcraft/Screenshots/WoWScrnShot_112116_001932.jpg|11/21/16 00:19:32|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [7]
+	"im losing my mind helppppppp aaaaaaaaaaaaaaah|World of Warcraft/Screenshots/WoWScrnShot_112116_001940.jpg|11/21/16 00:19:40|Avraeldinna|Dalaran|Shadowglen|Shadowglen", -- [8]
+}
