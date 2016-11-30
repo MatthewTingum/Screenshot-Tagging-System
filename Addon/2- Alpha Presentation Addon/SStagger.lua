701424377ec8500bd8c36f3526d40485256f2257
kaylobadboy <kristopher.r.lommen@my.und.edu>
2016-11-29 22:03:00 -0600

    Minor Code Revisions
    Updated Ver Number and Added in More Comments detailing the obtrusive
    segments of code

--- Addon/2- Alpha Presentation Addon/SStagger.lua
+++ Addon/2- Alpha Presentation Addon/SStagger.lua
@@ -1,5 +1,5 @@
-screenshotDB = screenshotDB or {}
-local settings = {}
+screenshotDB = screenshotDB or {} --Loads, or if not found, creates a db for outputting data logs.
+local settings = {} -- Placeholder table for user settings that will eventually be supported. (Custom User settings that save over restarts of the addon)
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("ADDON_LOADED")
 
@@ -9,15 +9,15 @@ end)
 
 
 SavedVar = {
-	text = "troll",
+	text = "teststring",
 	initialized = 0,
 }
-function runtime()
+function runtime() --Called whenever the hotkey is pressed from within the game
 MSGPrint()
 end
 
 
-function getdescript(arg, filenameha)
+function getdescript(arg, filenameha)--Creates all the UI frames and creates a database entry upon clicking the "cap tags" button on the main frame of the addon
 
 -- create a frame where we put our stuff
 local f=CreateFrame("Frame",nil,QuestLogDetailScrollChildFrame)
@@ -27,9 +27,6 @@ f:SetBackdrop({
       tile=1, tileSize=32, edgeSize=32, 
       insets={left=11, right=12, top=12, bottom=11}
 })
-
-
-
 
 f:IsResizable(true)
 
@@ -74,9 +71,6 @@ buttonexit:SetScript("OnClick", function(self) PlaySound("igMainMenuOption") sel
 
 local text = ""
 local text2=""
-
-
-
 f.text = f.text or f:CreateFontString(nil,"ARTWORK","QuestFont")
 f.text:SetTextColor(1,1,1,1)
 f.text:SetFont("Fonts\\FRIZQT__.TTF", 18)
@@ -84,21 +78,9 @@ f.text:SetText(format("SS Tagger"))
 f.text:SetPoint("CENTER", 0, 100)
 f.text:SetFontObject("ChatFontNormal")
 
---f.text2 = f.text2 or f:CreateFontString(nil,"ARTWORK","QuestFont")
---f.text2:SetTextColor(1,1,1,1)
---f.text2:SetText(format("Enter Description "))
---f.text2:SetPoint("CENTER", -100, 25)
---f.text2:SetFontObject("ChatFontNormal")
-
---f.text3 = f.text3 or f:CreateFontString(nil,"ARTWORK","QuestFont")
---f.text3:SetTextColor(1,1,1,1)
---f.text3:SetText(format("Enter Tags "))
---f.text3:SetPoint("CENTER", 100, 25)
---f.text3:SetFontObject("ChatFontNormal")
-
 f.text4 = f.text2 or f:CreateFontString(nil,"ARTWORK","QuestFont")
 f.text4:SetTextColor(1,1,1,1)
-f.text4:SetText(format("SStagger v 0.069\n11/22/16\nkaylo mathrax swagchard"))
+f.text4:SetText(format("SStagger v 0.69\n11/22/16\nkaylo mathrax swagchard"))
 f.text4:SetPoint("BOTTOMLEFT", 15, 15)
 f.text4:SetFont("Fonts\\FRIZQT__.TTF", 8)
 f.text4:SetFontObject("ChatFontNormal")
@@ -169,7 +151,7 @@ end
 
 
 
-function firsttime()
+function firsttime() --Upon first running the addon (When the game first loads or the UI is reset, the time is logged into the SStagger.lua file. This will allow for game sessions to be distinguished in the data sets in the future)
 
 local timevar = format(date())
 ChatFrame1:AddMessage("[SStagger]:Logged session start at "..timevar)
@@ -181,8 +163,8 @@ end
 function MSGPrint()
 
 local description = ""
-Screenshot()
-local timevar = format(date())
+local timevar = format(date()) --Time variable which will be used to create a filename format that is identical to the default naming mechanism that the game uses in the Screenshot() Function
+Screenshot()--built in function to take a SS and send it to the Wow/Screenshots directory
 local playerName = UnitName("player")
 local realmName =  GetRealmName()
 local ZoneText =  GetRealZoneText()
@@ -216,7 +198,7 @@ getdescript(description, filename)
 
 end
 
-function frame:ADDON_LOADED(AddOn)
+function frame:ADDON_LOADED(AddOn) --catches when the event of the addon being loaded is triggered, and runs this function
 if SavedVar.initialized == 0 then
 firsttime()
 end
