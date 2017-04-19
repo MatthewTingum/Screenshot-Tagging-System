d7190a0d12ae360c1edcfcc348cb99ed17ad4641
cartupa <richard.m.lee@ndus.edu>
2017-04-19 12:43:57 -0500

    javadocced

--- Client/CellDataEntry.java
+++ Client/CellDataEntry.java
@@ -1,13 +1,14 @@
-/**
- *Richard Lee
- *
- */
-
 import javax.swing.ImageIcon;
 import java.awt.Image;
 import java.net.URL;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+
+/**
+ * The CellDataEntry class stores all the information of an entry of the database.
+ * @author Richard Lee
+ * @version 2.0
+ */
 
 public class CellDataEntry {
  
@@ -18,7 +19,14 @@ public class CellDataEntry {
 	private String location;
 	private ImageIcon image;
 	
- 
+	/**
+	 * The constructor for the CellDataEntry object
+	 * @param t the tag of the entry
+	 * @param d the description of the entry
+	 * @param c the character name of the entry
+	 * @param l the loccation of the entry
+	 * @param i the path to the image to be used
+	 */
     public CellDataEntry(String t, String d, String c, String l, String i) {
         tag = t;
         desc = d;
@@ -28,26 +36,42 @@ public class CellDataEntry {
 		image = new ImageIcon(iconPath);
     }
  
+	/**
+	 * This get method returns the tag string value
+	 * @return tag The tag of the associated entry
+	 */
     public String getTag() {
         return tag;
     }
  
-    public void setTag(String t) {
-        tag = t;
-    }
- 
+	/**
+	 * This get method returns the description string value
+	 * @return desc The description of the associated entry
+	 */
     public String getDesc() {
         return desc;
     }
 	
+	/**
+	 * This get method returns the character name
+	 * @return character The character name associated with the entry
+	 */
 	public String getChar(){
 		return character;
 	}
 	
+	/**
+	 * This get method returns the location name
+	 * @return location The location name associated with the entry
+	 */
 	public String getLoc(){
 		return location;
 	}
 	
+	/**
+	 * This get method returns the ImageIcon
+	 * @return image The ImageIcon associated with the entry
+	 */
 	public ImageIcon getImage() {
 		return image;
 	}
--- Client/FileConfig.java
+++ Client/FileConfig.java
@@ -3,10 +3,21 @@ import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * The FileConfig class sets up a way to save information 
+ * @author Richard Lee
+ * @version 2.0
+ */ 
+
 public class FileConfig
 {
 	public static Properties fileConfig = new Properties();
 	
+	/**
+	 * This method saves a string values using a given name
+	 * @param value The string value to be saved
+	 * @param name The name of the value to be saved
+	 */
 	public void saveFileInfo(String value, String name)
 	{
 		try
@@ -20,6 +31,11 @@ public class FileConfig
 		}
 	}
 	
+	/** 
+	 * This method gets the value of a string saved under a given name
+	 * @param name The name of the value being accessed
+	 * @return value The value of the string being accessed
+	 */
 	public String getFileInfo(String name)
 	{
 		String value = "";
