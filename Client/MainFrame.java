dab8ab5143385767c7296cdba87b9df1f8022bc6
MatthewTingum <matthew.a.tingum@und.edu>
2017-04-19 18:02:33 -0500

    uhhhhh

--- Client/MainFrame.java
+++ Client/MainFrame.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 
-///*
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -37,9 +36,14 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
 import org.apache.commons.io.FileUtils;
-//*/
 
-//MainFrame is the main class that sorts between the different panels for the gui
+/**
+ * The MainFrame class is the main class that sorts between the different panels for the gui
+ * and contains some key methods
+ * @author Richard Lee
+ * @version 2.0
+ */ 
+ 
 public class MainFrame extends JFrame{
     
     //Uses a CardLayout to switch between different JPanels
@@ -52,23 +56,24 @@ public class MainFrame extends JFrame{
 	
     //Variables for purpose of connecting to database declared here
 	private static final String USER_AGENT = "Mozilla/5.0";
-
-	//private static final String GET_URL = "http://localhost:9090/SpringMVCExample";
-
 	private static final String POST_URL = "http://localhost:3000/api/submissions/submission";
-	
 	public String loginToken = "UA";
 	public boolean loggedIN = false;
 	
 	//File config stuff
 	FileConfig fCon = new FileConfig();
     
-    //Creates a new MainFrame object
+    /**
+	 * The main function called to start the GUI. Createss a new MainFrame object
+	 * @param args the input arguments, although it does nothing with them 
+	 */
     public static void main(String args[]) {
         new MainFrame();
     }
     
-    //Initializes the GUI's variables
+    /**
+	 * This constructor creates and returns an instance of the MainFrame object
+	 */
     public MainFrame() {
         super("SUPER AWESOME WOW APP");
         setResizable(false);
@@ -84,67 +89,58 @@ public class MainFrame extends JFrame{
         setVisible(true);
     }
     
-    //This function activates when a button is pushed that should take you to the Main Menu
+	/**
+	 * This function takes the user to the main menu 
+	 */
     public void showMain() {
 		setSize(820, 430);
         c.show(p, "mPage");
-		//menu.setLoginText();
     }
     
-    //This function activates when a button is pushed that should take you to the Search Page
+	/**
+	 * This function takes the user to the search screen 
+	 */
     public void showSearch() {
 		//setBounds(0, 0, 0, 0);
 		setSize(1100, 600);
         c.show(p, "sPage");
     }
 	
-	//This function activates when a button is pushed that should take you to the Login Page
+	/**
+	 * This function takes the user to the login page
+	 */
 	public void showLog() {
 		//setSize(820, 430);
 		c.show(p, "lPage");
 	}
 	
+	/**
+	 * This function takes the user to the help page 
+	 */
 	public void showHelp() {
 		c.show(p, "hPage");
 	}
 	
-	//*
+	/**
+	 * This function is called in order to find the World Of Warcraft directory and save the location
+	 */
 	public void findDirectory(){						
 		JFileChooser chooser = new JFileChooser(); 
 		chooser.setCurrentDirectory(new java.io.File("."));
 		chooser.setDialogTitle("Please select World of Warcraft Folder");
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-    //
-    // disable the "All files" option.
-    //
-		chooser.setAcceptAllFileFilterUsed(false);
-    //    
+    
+		chooser.setAcceptAllFileFilterUsed(false);  
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) { 
 			String directory = chooser.getSelectedFile().getAbsolutePath();
 			fCon.saveFileInfo(directory, "File");
 		}
 	}
 	
-	public void findScreenshotDir(){
-		JFileChooser chooser = new JFileChooser(); 
-		chooser.setCurrentDirectory(new java.io.File("."));
-		chooser.setDialogTitle("Please select folder for screenshots");
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-    //
-    // disable the "All files" option.
-    //
-		chooser.setAcceptAllFileFilterUsed(false);
-    //    
-		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) { 
-			String directory = chooser.getSelectedFile().getAbsolutePath();
-			fCon.saveFileInfo(directory, "Scrnshts");
-		}
-	}
-	//*/
-	
-	
-    //Function connects to database and attempts to send data to it
-	//*
+    /**
+	 * This function checks to see if it can find the World of Warcraft directory 
+	 * If it can, it calls the upload function, if not, it notifies the user 
+	 */
 	public void startSEND(){
 		String directory = fCon.getFileInfo("File");
 		File file1 = new File(directory + "\\WTF\\Account\\");
@@ -165,12 +161,14 @@ public class MainFrame extends JFrame{
 		}
 		
 	}
-	//*/
+	
+	/**
+	 * This function is the upload function.  It takes a string that contains info on the location
+	 * of the World of Warcraft directory and uses it to upload data to the database 
+	 * @param fileString the path to the correct directory 
+	 */
 	public void sendPOST(String fileString){
-		///*
 		System.out.println(fileString);
-		// Find and open a log file
-		//File file = new File("C:\\Program Files (x86)\\World of Warcraft\\WTF\\Account\\");
 		File file = new File(fileString);
 		if (!file.exists()){
 			System.out.println("File 1 doesn't exist");
@@ -178,7 +176,6 @@ public class MainFrame extends JFrame{
 		}
 		String[] account = file.list();
 		
-		//File file2 = new File("C:\\Program Files (x86)\\World of Warcraft\\WTF\\Account\\" + account[0] + "\\SavedVariables\\SStagger.lua");
 		File file2 = new File(fileString + "\\" + account[0] + "\\SavedVariables\\SStagger.lua");
 		System.out.println("File 2 Path: " + file2.getAbsolutePath());
 		if (!file2.exists()){
@@ -187,7 +184,6 @@ public class MainFrame extends JFrame{
 		}
 		else{
 			System.out.println("All is well");
-			//return;
 		}
 		BufferedReader reader2 = null;
 		List<String> list = new ArrayList<String>();
@@ -214,7 +210,7 @@ public class MainFrame extends JFrame{
 		}
 		
 		// Get all screenshot names (Must be done here due to limitations of WoW addon)
-		File screens = new File("C:\\Program Files (x86)\\World of Warcraft\\Screenshots\\");
+		File screens = new File(fileString + "\\..\\..\\Screenshots");
 		String[] scNames = screens.list();
 		
 		// Start with line 3 and ignore the last line
@@ -223,10 +219,8 @@ public class MainFrame extends JFrame{
 		
 		while (i < list.size() - 7){
 			
-			//System.out.println(list.get(i));
 			temp = list.get(i);
 			String[] splitData = temp.split("\\|");
-			//System.out.println(splitData[0]);
 		
 			System.out.println("Attempting to send post...\n");
 
@@ -234,12 +228,12 @@ public class MainFrame extends JFrame{
 			HttpPost httpPost = new HttpPost(POST_URL);
 			httpPost.addHeader("User-Agent", USER_AGENT);
 		
-			// This is where the user auth token will be (Using a static one for now -- linked to a test account)
 			httpPost.addHeader("Authorization", loginToken);
 
 			List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
 		
 			// Here is where we can add in any data to send to the database
+<<<<<<< HEAD
 			//urlParameters.add(new BasicNameValuePair("Title", "Troll gets rekt m8"));
 			
 			System.out.println(splitData.length);
@@ -249,18 +243,23 @@ public class MainFrame extends JFrame{
 				
 			}
 			
+=======
+>>>>>>> origin/master
 			urlParameters.add(new BasicNameValuePair("Description", splitData[1]));
 			urlParameters.add(new BasicNameValuePair("Tags", splitData[2]));
-			urlParameters.add(new BasicNameValuePair("Time", splitData[4]));		// Date/Time format still needs to be figured out
+			urlParameters.add(new BasicNameValuePair("Time", splitData[4]));	
 			urlParameters.add(new BasicNameValuePair("ImagePath", "app-content/images/" + scNames[i - 3]));
 			urlParameters.add(new BasicNameValuePair("Character", splitData[5]));
-			//urlParameters.add(new BasicNameValuePair("Server", splitData[5]));	// The server doesn't like chinese chars
 			urlParameters.add(new BasicNameValuePair("Location", splitData[7]));
+<<<<<<< HEAD
 			
 			urlParameters.add(new BasicNameValuePair("Chat", splitData[9]));
 
 			//urlParameters.add(new BasicNameValuePair("SubLocation", splitData[7]));	// This is problematic due to the way the addon delimits
 
+=======
+		
+>>>>>>> origin/master
 			try{
 				HttpEntity postParams = new UrlEncodedFormEntity(urlParameters);
 				httpPost.setEntity(postParams);
@@ -281,8 +280,6 @@ public class MainFrame extends JFrame{
 				}
 				reader.close();
 
-				// print result
-				//System.out.println(response.toString());
 				httpClient.close();
 			}catch (Exception e){
 				e.printStackTrace();
@@ -292,22 +289,8 @@ public class MainFrame extends JFrame{
 		}
 		
 		// Copy all images to server
-		File source = new File("C:\\Program Files (x86)\\World of Warcraft\\Screenshots");
-		//File dest = new File("C:\\Matt\\School\\Senioritus\\the one thing\\app\\app-content\\images");
+		File source = new File(fileString + "\\..\\..\\Screenshots");
 		File dest = new File("../Server/app/app-content/images");
-		//File dest = new File("C:\\Users\\kirby\\Desktop\\CSCI493\\github\\Screenshot-Tagging-System\\Server\\app\\app-content");
-		/*
-		// Upload images to imgur through the api
-		String imgurEndpoint = "https:\\api.imgur.com\\3\\image";
-		String clientID = "d56856daacda1ed";
-		
-		HttpPost httpPost2 = new HttpPost(POST_URL);
-		httpPost2.addHeader("User-Agent", USER_AGENT);
-		httpPost2.addHeader("Authorization", "Client-ID " . clientID);
-		
-		MultipartEntity reqEntity =  new MultipartEntity();
-		reqEntity.addPart("image", new FileBody(""));
-		*/
 		try {
 			FileUtils.copyDirectory(source, dest);
 		} catch (IOException e) {
@@ -323,9 +306,12 @@ public class MainFrame extends JFrame{
 		
 		// Clear log file
 		file2.delete();
-		//*/
 	}
 	
+	/**
+	 * This method takes a token and saves it and logs the user in 
+	 * @param token the token for accessing the database 
+	 */
 	public void logInUser(String token){
 		loginToken = token;
 		loggedIN = true;
@@ -333,28 +319,21 @@ public class MainFrame extends JFrame{
 		//System.out.println("Logging in");
 	}
 	
+	/**
+	 * This method logs the user out and takes them to the login page 
+	 */
 	public void logOutUser(){
 		loginToken = "UA";
 		loggedIN = false;
 		showLog();
 	}
 	
+	/**
+	 * This method returns the token for accessing the database 
+	 * @return loginToken token for accessing database 
+	 */
 	public String getToken(){
 		return loginToken;
 	}
 	
 }
-
-/*
-datebasename = {
-	"SESSION START AT TINE"
-	"tags|descript|filename|asdsad|asdsad|adsad|asdsad" [1]
-	
-	
-	
-	
-	
-	
-	
-}
-*/
